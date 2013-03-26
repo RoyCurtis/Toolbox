91e1931d8e2e81bce7d3d86eb0a47364d1a975ce
Roy Curtis <roy.adrian.curtis+github@gmail.com>
2013-03-26 21:09:57 +0000

    Added extensions for Strings and DateTime:
    - Terse splitting, case-insensitive equality check and cleaner char[] arrays for string.Split
    - SecondsToNow to quickly compare timespan between now and DateTime

---
+++ Extensions/DateTime.cs
@@ -0,0 +1,17 @@
+﻿namespace System
+{
+    /// <summary>
+    /// Provides useful extensions to DateTime
+    /// </summary>
+    public static class TBXDateTime
+    {
+        /// <summary>
+        /// Returns an integer amount of seconds difference between DateTime.Now and
+        /// this DateTime value
+        /// </summary>
+        public static int SecondsToNow(this DateTime time)
+        {
+            return (int) DateTime.Now.Subtract(time).TotalSeconds;
+        }
+    }
+}
---
+++ Extensions/Strings.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+
+namespace System
+{
+    /// <summary>
+    /// Provides useful extensions to strings
+    /// </summary>
+    public static class TBXString
+    {
+        /// <summary>
+        /// Shortcut to string.Split(char[]) that removes empty entries by default
+        /// </summary>
+        public static string[] TerseSplit( this string str, params char[] separators )
+        {
+            return str.Split( separators, StringSplitOptions.RemoveEmptyEntries );
+        }
+
+        /// <summary>
+        /// Shortcut to string.Split(string[]) that removes empty entries by default
+        /// </summary>
+        public static string[] TerseSplit( this string str, params string[] separators )
+        {
+            return str.Split( separators, StringSplitOptions.RemoveEmptyEntries );
+        }
+
+        /// <summary>
+        /// Case-insensitive string equality shortcut (current culture)
+        /// </summary>
+        public static bool IEquals ( this string source, string value )
+        {
+            return source.Equals( value, StringComparison.InvariantCultureIgnoreCase );
+        }
+    }
+
+    /// <summary>
+    /// Helper array for cleaner-looking operation of string.Split()
+    /// </summary>
+    public static class Split
+    {
+        /// <summary>
+        /// Helper array for string.Split for splitting by commas
+        /// </summary>
+        public static char[] Comma   = new[] { ',' };
+        /// <summary>
+        /// Helper array for string.Split for splitting by dots
+        /// </summary>
+        public static char[] Dot     = new[] { '.' };
+        /// <summary>
+        /// Helper array for string.Split for splitting by forward slashes
+        /// </summary>
+        public static char[] FSlash  = new[] { '/' };
+        /// <summary>
+        /// Helper array for string.Split for splitting by backward slashes
+        /// </summary>
+        public static char[] BSlash  = new[] { '\\' };
+        /// <summary>
+        /// Helper array for string.Split for splitting by both slashes
+        /// </summary>
+        public static char[] Slashes = new[] { '/', '\\' };
+    }
+}
