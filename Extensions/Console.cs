c61c83465cf6f4a0f5fc3bc8a73e1b49f3e6bca5
Roy Curtis <roy.adrian.curtis+github@gmail.com>
2013-03-29 14:56:27 +0000

    - added WriteColored and WriteHighlighted for Console.Write
    - minor code cleanup

--- Extensions/Console.cs
+++ Extensions/Console.cs
@@ -5,17 +5,32 @@
     /// </summary>
     public static class TBXConsole
     {
-        static object lockkey = new object();
+        static object mutex = new object();
 
-        /// <summary>
+         /// <summary>
         /// Writes colored text to the console, resetting the foreground color to the
         /// previous value
         /// </summary>
+        public static void WriteColored(ConsoleColor color, string message, params object[] args)
+        {
+            lock (mutex)
+            {
+                var prevColor           = Console.ForegroundColor;
+                Console.ForegroundColor = color;
+                Console.Write(message, args);
+                Console.ForegroundColor = prevColor;
+            }
+        }
+
+        /// <summary>
+        /// Writes colored text lines to the console, resetting the foreground color to
+        /// the previous value
+        /// </summary>
         public static void WriteLineColored(ConsoleColor color, string message, params object[] args)
         {
-            lock (lockkey)
+            lock (mutex)
             {
-                var prevColor = Console.ForegroundColor;
+                var prevColor           = Console.ForegroundColor;
                 Console.ForegroundColor = color;
                 Console.WriteLine(message, args);
                 Console.ForegroundColor = prevColor;
@@ -26,11 +41,26 @@
         /// Writes highlighted text to the console, resetting the background color to the
         /// previous value
         /// </summary>
+        public static void WriteHighlighted(ConsoleColor bgcolor, string message, params object[] args)
+        {
+            lock (mutex)
+            {
+                var prevColor           = Console.BackgroundColor;
+                Console.BackgroundColor = bgcolor;
+                Console.Write(message, args);
+                Console.BackgroundColor = prevColor;
+            }
+        }
+
+        /// <summary>
+        /// Writes highlighted text lines to the console, resetting the background color
+        /// to the previous value
+        /// </summary>
         public static void WriteLineHighlighted(ConsoleColor bgcolor, string message, params object[] args)
         {
-            lock (lockkey)
+            lock (mutex)
             {
-                var prevColor = Console.BackgroundColor;
+                var prevColor           = Console.BackgroundColor;
                 Console.BackgroundColor = bgcolor;
                 Console.WriteLine(message, args);
                 Console.BackgroundColor = prevColor;
@@ -41,12 +71,30 @@
         /// Writes both highlighted and colored text to the console, resetting both values
         /// to the previous
         /// </summary>
+        public static void WriteColored(ConsoleColor bgcolor, ConsoleColor textcolor, string message, params object[] args)
+        {
+            lock (mutex)
+            {
+                var prevBGColor         = Console.BackgroundColor;
+                var prevFGColor         = Console.ForegroundColor;
+                Console.BackgroundColor = bgcolor;
+                Console.ForegroundColor = textcolor;
+                Console.Write(message, args);
+                Console.BackgroundColor = prevBGColor;
+                Console.ForegroundColor = prevFGColor;
+            }
+        }
+
+        /// <summary>
+        /// Writes both highlighted and colored text lines to the console, resetting both
+        /// values to the previous
+        /// </summary>
         public static void WriteLineColored(ConsoleColor bgcolor, ConsoleColor textcolor, string message, params object[] args)
         {
-            lock (lockkey)
+            lock (mutex)
             {
-                var prevBGColor = Console.BackgroundColor;
-                var prevFGColor = Console.ForegroundColor;
+                var prevBGColor         = Console.BackgroundColor;
+                var prevFGColor         = Console.ForegroundColor;
                 Console.BackgroundColor = bgcolor;
                 Console.ForegroundColor = textcolor;
                 Console.WriteLine(message, args);
